89a29e8171a6b54966f0a059334dfc31c8521e54
Emre Turan <93795815+emreturan00@users.noreply.github.com>
2023-04-17 14:39:06 +0300

    kuzey versiyon

---
+++ Views/Home/MainIndex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DurdenTyler.Views.Home
+{
+    public class MainIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
